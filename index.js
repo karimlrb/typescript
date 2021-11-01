adc770c7101ff5d95ea81610d86642750711cdd2
karimlrb <japan-rimk@hotmail.fr>
2021-11-01 03:16:32 +0100

    première manip avec typescript

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+// variables
+var num1 = 24;
+var num2;
+var numOrString;
+num2 = 45;
+numOrString = 475;
+// Tableaux
+var arr = ["chien", "cheval", "poisson"];
+var tableau1 = [];
+var tableau2 = [];
+tableau1;
+tableau2.push(false, 1, 2, 3);
+tableau1.push("tigre");
+console.log(tableau2);
+console.log(tableau1);
--- index.ts
+++ index.ts
@@ -1,7 +1,24 @@
+// variables
 let num1 = 24;
 let num2:number;
-let numOrString:number|string;
+let numOrString: number | string;
 
 num2 = 45;
 
 numOrString = 475;
+
+// Tableaux
+
+let arr = ["chien", "cheval", "poisson"];
+
+let tableau1: string[] = [];
+let tableau2: (number | boolean) [] = [];
+
+tableau1
+
+tableau2.push(false,1,2,3);
+tableau1.push("tigre");
+
+console.log(tableau2);
+console.log(tableau1);
+
