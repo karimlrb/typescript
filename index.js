f17a890bd50c08faf9fbc0316075c2fb456d9458
karimlrb <japan-rimk@hotmail.fr>
2021-11-01 05:33:47 +0100

    interface, fonctions, heritage exos

--- index.js
+++ index.js
@@ -12,14 +12,12 @@ var tableau2 = [];
 tableau1;
 tableau2.push(false, 1, 2, 3);
 tableau1.push("tigre");
-console.log(tableau2);
-console.log(tableau1);
 var zidane = {
     id: 0,
     name: "Zidane",
     jersey: 10
 };
-console.log(zidane);
+// console.log(zidane);
 // Les classes
 var Singer = /** @class */ (function () {
     function Singer(id, name, alive) {
@@ -29,5 +27,55 @@ var Singer = /** @class */ (function () {
     }
     return Singer;
 }());
+// Si le "alive" est optionnel il faut aussi rajouer le "?" dans la class
 var drake = new Singer(0, "Obraham");
-console.log(drake);
+// console.log(drake);
+// Les fonctions
+var sayMyname = function (name) {
+    // console.log(`Bonjour ${name}`);
+};
+sayMyname();
+var ageDuCapitaine = function (age, size) {
+    if (size) {
+        console.log("Le capitaine a " + age + " ans et mesure " + size + " cm");
+    }
+    else {
+        console.log("Le capitaine a " + age + " ans");
+    }
+};
+// ageDuCapitaine(29, 192);
+//  Tuple
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["Premium"] = 1] = "Premium";
+    Role[Role["Freemium"] = 2] = "Freemium";
+})(Role || (Role = {}));
+// Enum
+var user1 = {
+    name: "zizou",
+    attributes: [29, "yazid"],
+    role: Role.Premium
+};
+var user3 = {
+    name: "Jessica",
+    age: 27,
+    job: "Chef de projet React",
+    diplomes: 3
+};
+// console.log(user3);
+// Si on veut declarer user  mais qu'in ne veut pas encore declarer les infos dans l'interface
+// const user4 = <Femme>{}
+// Permet de declarer un objet vide avec l'interface Femme
+var user4 = {};
+user4.name = "Jean";
+var sayHello = function (name, age) {
+    console.log(" Hello " + name + ", tu as " + age + " ans");
+};
+var bestFriend = function (name) { return name; };
+// La ligne du dessus correspond à ça
+// const bestFriend:Hello = (name)=>{
+//     return name
+// }
+sayHello('Yves', 29);
+console.log("L'ami de Jean s'apelle " + bestFriend("Luc"));
--- index.ts
+++ index.ts
@@ -19,8 +19,8 @@ tableau1
 tableau2.push(false,1,2,3);
 tableau1.push("tigre");
 
-console.log(tableau2);
-console.log(tableau1);
+// console.log(tableau2);
+// console.log(tableau1);
 
 // Objets
 
@@ -36,7 +36,7 @@ const zidane:Player = {
     jersey: 10
 };
 
-console.log(zidane);
+// console.log(zidane);
 
 // Les classes
 
@@ -51,6 +51,93 @@ class Singer {
         this.alive = alive;
     }
 }
-
+// Si le "alive" est optionnel il faut aussi rajouer le "?" dans la class
 const drake = new Singer(0,"Obraham");
-console.log(drake);
+// console.log(drake);
+
+// Les fonctions
+
+const sayMyname = (name?: string) => {
+    // console.log(`Bonjour ${name}`);
+    
+}
+
+sayMyname();
+
+const ageDuCapitaine = (age: number|string, size?:number):void => {
+    if(size){
+        console.log(`Le capitaine a ${age} ans et mesure ${size} cm`);  
+    }
+    else {
+        console.log(`Le capitaine a ${age} ans`);
+    }
+}
+
+// ageDuCapitaine(29, 192);
+
+//  Tuple
+enum Role {Admin, Premium, Freemium}
+
+interface User {
+    name: string;
+    attributes:[number, string];
+    role : Role;
+}
+
+// Enum
+
+const user1:User = {
+    name : "zizou", 
+    attributes: [29, "yazid"],
+    role:Role.Premium
+}
+
+// console.log(user1);
+
+// Heritage d'interface---------------
+
+// Ici l'heritage d'interface sert aux objets, ne pas confondre avec les classes
+
+
+interface Humain{
+    name:string;
+    age:number;
+} 
+
+interface Femme extends Humain{
+    job:string;
+    diplomes:number
+}
+
+const user3:Femme = {
+    name:"Jessica",
+    age:27,
+    job:"Chef de projet React",
+    diplomes:3
+}
+// console.log(user3);
+
+// Si on veut declarer user  mais qu'in ne veut pas encore declarer les infos dans l'interface
+// const user4 = <Femme>{}
+// Permet de declarer un objet vide avec l'interface Femme
+
+const user4 = <Femme> {};
+user4.name = "Jean";
+
+// console.log(user4.name);
+interface Hello {
+    (name:string, age?:number): void;
+}
+
+const sayHello: Hello = (name, age) => {
+    console.log(` Hello ${name}, tu as ${age} ans`);
+}
+
+const bestFriend: Hello = name=>name;
+// La ligne du dessus correspond à ça
+// const bestFriend:Hello = (name)=>{
+//     return name
+// }
+
+sayHello('Yves', 29);
+console.log(`L'ami de Jean s'apelle ${bestFriend("Luc")}`);
